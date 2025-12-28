06c1b9a1730f96e7e6dfae273b163a28f8c4d99c
Soltysova <kontakt.soltysovaberta@gmail.com>
2025-12-28 19:29:29 +0100

    Update modul_test.js

--- js/modul_test.js
+++ js/modul_test.js
@@ -1,5 +1,5 @@
-import * as THREE from 'unpkg.com';
-import { GLTFLoader } from 'unpkg.com';
+import * as THREE from 'https://unpkg.com/three@0.158.0/build/three.module.js';
+import { GLTFLoader } from 'https://unpkg.com/three@0.158.0/examples/jsm/loaders/GLTFLoader.js';
 
 const container = document.getElementById('model');
 
