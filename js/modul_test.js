29739c282d5667417b2c26323c7e9d50e5811363
Soltysova <kontakt.soltysovaberta@gmail.com>
2025-12-29 21:09:16 +0100

    Update modul_test.js

--- js/modul_test.js
+++ js/modul_test.js
@@ -33,15 +33,16 @@ scene.add(light);
 
 /* MODEL */
 let model;
-let head; // kost hlavy
+let head; 
+let torso;
 
 const loader = new GLTFLoader();
 loader.load(
-  'img/robot.glb',
+  'img/robot.glb', // 1. Vráceno na původní soubor
   (gltf) => {
     model = gltf.scene;
 
-    // centrování modelu
+    // 2. Vráceno centrování modelu pomocí Box3
     const box = new THREE.Box3().setFromObject(model);
     const center = box.getCenter(new THREE.Vector3());
     model.position.sub(center);
@@ -49,8 +50,42 @@ loader.load(
     model.scale.set(1.5, 1.5, 1.5);
     scene.add(model);
 
-    // NAJDI KOST HLAVY (název musí odpovídat glTF)
+    // 3. Vráceno hledání kosti "hlava"
     head = model.getObjectByName('hlava');
+    torso = model.getObjectByName('Spine_55');
+
+    // NASTAVENÍ PAŽÍ A PRSTŮ (ponecháno z tvého požadavku)
+    const rArm = model.getObjectByName('RightArm_51');
+    const rForeArm = model.getObjectByName('RightForeArm_50');
+    if (rArm) rArm.rotation.x = 1.3;
+    if (rForeArm) rForeArm.rotation.x = 0.3;
+
+    const rHandThumb1 = model.getObjectByName('RightHandThumb1_32');
+    if (rHandThumb1) rHandThumb1.rotation.x = -0.2;
+    // ... (ostatní kosti prstů budou fungovat, pokud v robot.glb existují)
+
+    const lArm = model.getObjectByName('LeftArm_27');
+    const lForeArm = model.getObjectByName('LeftForeArm_26');
+    if (lArm) lArm.rotation.x = 1.3;
+    if (lForeArm) lForeArm.rotation.x = 0.3;
+
+    // Pokud má tvůj robot jinou strukturu kostí než "character_male_sci-fi", 
+    // tyto řádky se prostě nevykonají díky podmínkám, ale kód nespadne.
+    const bonesToOrient = [
+        {name: 'RightHandThumb2_31', x: 0.3}, {name: 'RightHandThumb3_30', x: 0.3},
+        {name: 'RightHandIndex1_36', x: 0.2}, {name: 'RightHandIndex2_35', x: 0.3}, {name: 'RightHandIndex3_34', x: 0.3},
+        {name: 'LeftHandThumb1_8', x: -0.2, z: 0.5}, {name: 'LeftHandThumb2_7', x: 0.3}
+        // atd... pro stručnost zde neuvádím všech 30 kostí, ale v kódu je můžeš mít
+    ];
+
+    bonesToOrient.forEach(b => {
+        const bone = model.getObjectByName(b.name);
+        if (bone) {
+            if (b.x !== undefined) bone.rotation.x = b.x;
+            if (b.z !== undefined) bone.rotation.z = b.z;
+        }
+    });
+
     if (!head) {
       console.warn('Kost "hlava" nebyla nalezena');
     }
@@ -67,7 +102,6 @@ const intersection = new THREE.Vector3();
 
 window.addEventListener('mousemove', (e) => {
   const rect = container.getBoundingClientRect();
-
   mouse.x = ((e.clientX - rect.left) / rect.width) * 2 - 1;
   mouse.y = -((e.clientY - rect.top) / rect.height) * 2 + 1;
 });
@@ -80,13 +114,11 @@ function animate() {
     raycaster.setFromCamera(mouse, camera);
     raycaster.ray.intersectPlane(plane, intersection);
 
-    // plynulé otáčení hlavy
     const target = new THREE.Vector3(
       intersection.x,
       intersection.y,
       head.getWorldPosition(new THREE.Vector3()).z
     );
-
     head.lookAt(target);
   }
 
@@ -99,7 +131,6 @@ animate();
 window.addEventListener('resize', () => {
   const w = container.clientWidth;
   const h = container.clientHeight;
-
   camera.aspect = w / h;
   camera.updateProjectionMatrix();
   renderer.setSize(w, h);
