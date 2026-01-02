66ff813f17d38316eeab2cddc3a2e195f7960248
Soltysova <kontakt.soltysovaberta@gmail.com>
2026-01-02 15:32:56 +0100

    mirne opravy

--- js/robot-kostra.js
+++ js/robot-kostra.js
@@ -47,7 +47,7 @@ function initThreeJS() {
             const scaleValue = 2.5; // Zpět na původní, větší měřítko
             model.scale.set(scaleValue, scaleValue, scaleValue);
             model.position.y = -2.2; 
-            model.position.x = -0.5; // Posun doleva zachován
+            model.position.x = 0; // Posun doleva zachován
 
             scene.add(model);
         },
